a74a68553fc1ce430ce5d95fa164864f9e16a3e1
tekhuy <huyle20@hotmail.com>
2014-12-12 11:57:34 +0000

    created files

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,5 @@
-require 'airport'
-require 'plane'
+# require 'airport'
+# require 'plane'
 
 # A plane currently in the airport can be requested to take off.
 #
@@ -7,6 +7,7 @@ require 'plane'
 # It is up to you how many planes can land in the airport and how that is impermented.
 #
 # If the airport is full then no planes can land
+class Airport
 
 describe Airport do
 
@@ -55,38 +56,4 @@ end
 # When we land a plane at the airport, the plane in question should have its status changed to "landed"
 #
 # When the plane takes of from the airport, the plane's status should become "flying"
-
-describe Plane do
-
-  let(:plane) { Plane.new }
-
-  it 'has a flying status when created' do
-
-  end
-
-  it 'has a flying status when in the air' do
-
-  end
-
-  it 'can take off' do
-
-  end
-
-  it 'changes its status to flying after taking of' do
-
-  end
-end
-
-# grand final
-# Given 6 planes, each plane must land. When the airport is full, every plane must take off again.
-# Be careful of the weather, it could be stormy!
-# Check when all the planes have landed that they have the right status "landed"
-# Once all the planes are in the air again, check that they have the status of flying!
-
-describe "The gand finale (last spec)" do
-
-  it 'all planes can land and all planes can take off' do
-
-  end
-end
 
---
+++ spec/plane_spec.rb
@@ -0,0 +1,38 @@
+# require 'airport'
+# require 'plane'
+
+
+describe Plane do
+
+  let(:plane) { Plane.new }
+
+  it 'has a flying status when created' do
+
+  end
+
+  it 'has a flying status when in the air' do
+
+  end
+
+  it 'can take off' do
+
+  end
+
+  it 'changes its status to flying after taking of' do
+
+  end
+end
+
+# grand final
+# Given 6 planes, each plane must land. When the airport is full, every plane must take off again.
+# Be careful of the weather, it could be stormy!
+# Check when all the planes have landed that they have the right status "landed"
+# Once all the planes are in the air again, check that they have the status of flying!
+
+describe "The gand finale (last spec)" do
+
+  it 'all planes can land and all planes can take off' do
+
+  end
+end
+
